671b7b8c29e44cbe7bd3e91242df459078046822
norbertkrzeminski <krzeminskin.01@gmail.com>
2020-06-06 14:54:59 +0200

    javascript course beginning

---
+++ javascript_course/sandbox.js
@@ -0,0 +1,10 @@
+// alert('Hello world ');
+// variables
+let age=28;
+let year=2020;
+console.log(age, year);
+age=30; //change of value of variable
+console.log(age);
+// const
+const points=100;
+console.log(points);
