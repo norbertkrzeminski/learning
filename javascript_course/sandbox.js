046e9e8b9116d79decb5720db0b140ae01c2a1bd
norbertkrzeminski <krzeminskin.01@gmail.com>
2020-06-10 11:19:08 +0200

    javascript course types pt2

--- javascript_course/sandbox.js
+++ javascript_course/sandbox.js
@@ -54,11 +54,47 @@ console.log(radius, pi);
 // let result3 = pi * radius**2;
 
 // console.log(result3);
-let likes = 10;
+// let likes = 10;
 // likes++;
 // likes--;
 // likes+=10;
 // console.log(likes);
 //Not a Number - NaN
 console.log(5/'hello');
-let result3 = 'the blog has ' + likes + " likes";
+// let result3 = 'the blog has ' + likes + " likes";
+//string template
+const tittle = 'Best reads of 2020';
+const author = 'Mario';
+const likes = 30;
+//concatenation way
+// let result4 = 'The blog called ' + tittle + 'by ' + author + ' has ' + likes + ' likes';
+// console.log(result4);
+//template way
+let result4 = `The blog called ${tittle} by ${author} has ${likes} likes`;
+console.log(result4);
+// creating html template
+let html = `
+    <h2>${tittle}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes.</span>
+`;
+console.log(html);
+//arrays
+let ninjas = ['shaun','ryu', 'hun-li'];
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+// let ages = [20,25,30,35];
+// console.log(ages[2]);
+// let random = ['shaun', 'crystal', 30, 20];
+// console.log(random);
+console.log(ninjas.length);
+// array methods
+// let resultArray = ninjas.join(',');
+// let resultArray = ninjas.indexOf('shaun');
+// let resultArray = ninjas.concat(['ken', 'crystal']);
+let resultArray = ninjas.push('ken');
+resultArray = ninjas.pop();
+console.log(resultArray);
+//NULL
+let newAge = null;
+console.log(newAge, newAge + 3, 'The age is ${newAge} ' );
