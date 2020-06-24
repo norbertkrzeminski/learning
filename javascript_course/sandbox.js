ebb7322a8c4b6f2fe1ecce4ca4c219ed9bedfa25
norbertkrzeminski <krzeminskin.01@gmail.com>
2020-06-24 10:02:31 +0200

    javascript course types pt4

--- javascript_course/sandbox.js
+++ javascript_course/sandbox.js
@@ -107,7 +107,50 @@ console.log(score + 1);
 console.log(typeof score);
 
 // for loops
-for(let i=0; i<5;i++){
-    console.log('in loop: ', i)
+// for(let i=0; i<5;i++){
+//     console.log('in loop: ', i)
+// }
+// console.log('loop finished')
+
+const anotherNames = ['mario', 'luigi' ,'toad'];
+// for(let i=0; i < anotherNames.length; i++){
+//     // console.log(anotherNames[i]);
+//     let html = `<div>${anotherNames[i]}</div>`
+//     console.log(html)
+// }
+// while loops
+// let i = 0
+// while(i<5){
+// console.log('in loop ', i);
+// i++;
+// }
+// while(i<anotherNames.length){
+//     console.log(anotherNames[i]);
+//     i++;
+// }
+// do while loops
+let i = 5;
+do {
+    console.log('The value of i is', i);
+    i++;
+    }
+while(i<5);
+// if statements
+// const ifAge = 25;
+// if(ifAge>20){
+//     console.log('You are over 20 years old');
+// }
+
+// const newNinjas = ['shaun','ryu', 'hun-li', 'yoshi'];
+// if(newNinjas.length>3){
+//     console.log("that's a lot of ninjas")
+// }
+
+const password = 'p@ssword1234';
+if(password.length >=12){
+    console.log("it's strong password")
+} else  if(password.length >= 8){
+    console.log("that password is long enough!");
+} else {
+    console.log('password is to short')
 }
-console.log('loop finished');
